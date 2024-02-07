069fb545a744c74d4d56e2d52c6be6e885301497
Your Name <mdrmatos43@gmail.com>
2024-02-06 21:52:06 -0300

    ok

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceBinanceService } from '../service/service-binance.service';
+import { getCurrencySymbol, getLocaleCurrencyName } from '@angular/common';
 
 
 
@@ -23,14 +24,22 @@ export class MonitorComponent implements OnInit {
   ngOnInit(): void{
     this.ExibirDados.getTickerPrice('BTCUSDT').subscribe(
       (data: any) => {
+          data.a = parseFloat(data.a).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 });
+            this.monitor_de_dados = data;        
+      
+      
 
-        
-        data.a = parseFloat(data.a).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 });
-        
 
-        this.monitor_de_dados = data;
-        
-      },
+
+
+          },
+
+
+
+
+
+
+
       (error: any) => {
       return  error;
       }
